efd8b6fd60f0bb3e4e20ba9ae712a58213eecbdb
naitiksharma014 <naitiksharma014@gmail.com>
2025-12-26 21:07:24 +0530

    Add brute force and start binary search approach
    Implemented a brute force approach and started a binary search approach for finding the kth missing positive number.

--- Difficulty: Medium/Kth Missing Positive Number in a Sorted Array/kth-missing-positive-number-in-a-sorted-array.java
+++ Difficulty: Medium/Kth Missing Positive Number in a Sorted Array/kth-missing-positive-number-in-a-sorted-array.java
@@ -1,3 +1,42 @@
+//---------------------------------------------------------------( APPROACH - 1 )-------------------------------------------------------------------------------
+//TC : O(n)
+//Brute Force
+
+class Solution {
+    public int kthMissing(int[] arr, int k) {
+        // code here
+        int n = arr.length;
+        int j = 0;
+        int count = 0;
+        
+        for(int i = 1; i <= arr[n - 1]; i++){
+           
+          
+            if(j < n && i == arr[j]){
+                
+                j++;
+            }
+            
+            else{
+                
+                k--;
+                
+                if(k == 0) return i;
+            }
+           
+        }
+        
+        return arr[n - 1] + k;
+    }
+}
+
+
+
+
+//---------------------------------------------------------------( APPROACH - 1 )-------------------------------------------------------------------------------
+//TC : O(log n)
+//BINARY SEARCH
+
 class Solution {
     public int kthMissing(int[] arr, int k) {
         // code here
