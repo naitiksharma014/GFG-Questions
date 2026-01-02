4b726077b12a2baf8ae03612e6d915b72c8001e6
naitiksharma014 <naitiksharma014@gmail.com>
2026-01-02 21:21:59 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.java
+++ Difficulty: Medium/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.java
@@ -1,32 +1,40 @@
 class Solution {
     public void sort012(int[] arr) {
         // code here
-        int zeros = 0, ones = 0, twos = 0;
+        int n = arr.length;
         
-        for(int num: arr){
-            
-            if(num == 0) zeros++;
-            else if(num == 1) ones++;
-            else twos++;
-        }
+        int low = 0, mid = 0, high = n - 1;
         
-        int i = 0;
-        while(zeros > 0){
+        while(mid <= high){
             
-            arr[i++] = 0;
-            zeros--;
-        }
-        
-        while(ones > 0){
+            if(arr[mid] == 0){
+                
+                //0 should be at starting
+                //SWAP -> Mid and Low
+                int temp = arr[mid];
+                arr[mid] = arr[low];
+                arr[low] = temp;
+                
+                low++;
+                mid++;
+            }
             
-            arr[i++] = 1;
-            ones--;
-        }
-        
-        while(twos > 0){
+            else if(arr[mid] == 1){
+                
+                //1 should be at middle, no action required
+                mid++;
+            }
             
-            arr[i++] = 2;
-            twos--;
+            else{
+                
+                //2 should be at last
+                //SWAP -> Mid and High
+                int temp = arr[mid];
+                arr[mid] = arr[high];
+                arr[high] = temp;
+                
+                high--;
+            }
         }
     }
 }
