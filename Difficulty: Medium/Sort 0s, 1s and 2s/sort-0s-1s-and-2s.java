399668f429f83c6e644287cbb02f9c3c8a5e28b0
naitiksharma014 <naitiksharma014@gmail.com>
2026-01-02 21:26:48 +0530

    sorting algorithm for 0s, 1s, and 2s with 2 approach
    Implemented two approaches for sorting an array of 0s, 1s, and 2s using the Dutch National Flag Algorithm and a counting method.

--- Difficulty: Medium/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.java
+++ Difficulty: Medium/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.java
@@ -1,3 +1,8 @@
+//---------------------------------------------------------------(APPROACH - 1)---------------------------------------------------------------------------------
+//Dutch National Flag Algorithm
+//TC: O(n) with one pass
+//Optimal
+
 class Solution {
     public void sort012(int[] arr) {
         // code here
@@ -35,6 +40,48 @@ class Solution {
                 
                 high--;
             }
+        }
+    }
+}
+
+
+
+
+
+//---------------------------------------------------------------(APPROACH - 2)---------------------------------------------------------------------------------
+//TC: O(n) with multiple pass
+
+
+
+class Solution {
+    public void sort012(int[] arr) {
+        // code here
+        int zeros = 0, ones = 0, twos = 0;
+        
+        for(int num: arr){
+            
+            if(num == 0) zeros++;
+            else if(num == 1) ones++;
+            else twos++;
+        }
+        
+        int i = 0;
+        while(zeros > 0){
+            
+            arr[i++] = 0;
+            zeros--;
+        }
+        
+        while(ones > 0){
+            
+            arr[i++] = 1;
+            ones--;
+        }
+        
+        while(twos > 0){
+            
+            arr[i++] = 2;
+            twos--;
         }
     }
 }
